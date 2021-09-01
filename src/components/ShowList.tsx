fa332a9608335f7d2b35d8b8529959676ede8a1a
Sabrina Evans <sabrina.evans997@hotmail.co.uk>
2021-09-01 12:13:04 +0100

    Change workflows to reflect lack of tests

--- src/components/ShowList.tsx
+++ src/components/ShowList.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Show from "./Show";
 import episodes from "../episodes.json";
 import { IEpisode } from "../interfaces/Episode";
@@ -9,10 +9,27 @@ const listStyles = {
 
 function ShowList(): JSX.Element {
   const [searchTerm, setSearchTerm] = useState("");
+  const [selectedEpisode, setSelectedEpisode] = useState<IEpisode>();
+  const [filteredEpisodes, setFilteredEpisodes] =
+    useState<IEpisode[]>(episodes);
 
-  // add useEffect to filter list when searchTerm changes
-  // add dropdown
-  // add counter
+  useEffect(() => {
+    const newFilteredEpisodes = episodes.filter(
+      (episode) =>
+        episode.summary.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        episode.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setFilteredEpisodes(newFilteredEpisodes);
+  }, [searchTerm]);
+
+  useEffect(() => {
+    if (selectedEpisode) {
+      setFilteredEpisodes([selectedEpisode]);
+    } else {
+      setFilteredEpisodes(episodes);
+    }
+  }, [selectedEpisode]);
 
   return (
     <div>
@@ -20,33 +37,36 @@ function ShowList(): JSX.Element {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
+      <div></div>
+      <select>
+        {filteredEpisodes.map((episode: IEpisode) => {
+          <option value={episode.name}>{episode.name}</option>;
+        })}
+      </select>
+      {filteredEpisodes.length !== episodes.length && (
+        <p>
+          Showing {filteredEpisodes.length} of {episodes.length} episodes
+        </p>
+      )}
       <section style={listStyles}>
-        {episodes
-          .filter(
-            (episode) =>
-              episode.summary
-                .toLowerCase()
-                .includes(searchTerm.toLowerCase()) ||
-              episode.name.toLowerCase().includes(searchTerm.toLowerCase())
-          )
-          .map((episode: IEpisode) => (
-            <Show
-              key={episode.id}
-              id={episode.id}
-              url={episode.url}
-              name={episode.name}
-              season={episode.season}
-              number={episode.number}
-              type={episode.type}
-              airdate={episode.airdate}
-              airtime={episode.airtime}
-              airstamp={episode.airstamp}
-              runtime={episode.runtime}
-              image={episode.image}
-              summary={episode.summary}
-              _links={episode._links}
-            />
-          ))}
+        {filteredEpisodes.map((episode: IEpisode) => (
+          <Show
+            key={episode.id}
+            id={episode.id}
+            url={episode.url}
+            name={episode.name}
+            season={episode.season}
+            number={episode.number}
+            type={episode.type}
+            airdate={episode.airdate}
+            airtime={episode.airtime}
+            airstamp={episode.airstamp}
+            runtime={episode.runtime}
+            image={episode.image}
+            summary={episode.summary}
+            _links={episode._links}
+          />
+        ))}
       </section>
     </div>
   );
