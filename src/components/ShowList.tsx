a14e94149d05ae713c623142309887b50a1f974a
Sabrina Evans <sabrina.evans997@hotmail.co.uk>
2021-09-01 14:28:05 +0100

    Fetch data from API

--- src/components/ShowList.tsx
+++ src/components/ShowList.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Show from "./Show";
-import episodes from "../episodes.json";
+// import episodes from "../episodes.json";
 import { IEpisode } from "../interfaces/Episode";
 import getFullEpisodeNumber from "../utils/EpisodeNumber";
 
@@ -11,30 +11,38 @@ const listStyles = {
 function ShowList(): JSX.Element {
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedEpisode, setSelectedEpisode] = useState<IEpisode>();
-  const [filteredEpisodes, setFilteredEpisodes] =
-    useState<IEpisode[]>(episodes);
+  const [filteredEpisodes, setFilteredEpisodes] = useState<IEpisode[]>([]);
+  const [allEpisodes, setAllEpisodes] = useState<IEpisode[]>([]);
 
   const handleClear = () => {
     setSelectedEpisode(undefined);
   };
 
+  const showCode = "82"; // Game of Thrones
+
   useEffect(() => {
-    const newFilteredEpisodes = episodes.filter(
+    fetch(`https://api.tvmaze.com/shows/${showCode}/episodes`)
+      .then((res) => res.json())
+      .then((jsonBody: IEpisode[]) => setAllEpisodes(jsonBody));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredEpisodes = allEpisodes.filter(
       (episode) =>
         episode.summary.toLowerCase().includes(searchTerm.toLowerCase()) ||
         episode.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     setFilteredEpisodes(newFilteredEpisodes);
-  }, [searchTerm]);
+  }, [searchTerm, allEpisodes]);
 
   useEffect(() => {
     if (selectedEpisode) {
       setFilteredEpisodes([selectedEpisode]);
     } else {
-      setFilteredEpisodes(episodes);
+      setFilteredEpisodes(allEpisodes);
     }
-  }, [selectedEpisode]);
+  }, [selectedEpisode, allEpisodes]);
 
   return (
     <div>
@@ -46,21 +54,21 @@ function ShowList(): JSX.Element {
       <select
         onChange={(e) =>
           setSelectedEpisode(
-            episodes.filter((episode) => episode.name === e.target.value)[0]
+            allEpisodes.filter((episode) => episode.name === e.target.value)[0]
           )
         }
       >
         {/* TODO: Find a better way to select a single episode in case of name clashes */}
-        {episodes.map((episode: IEpisode, index) => (
+        {allEpisodes.map((episode: IEpisode, index) => (
           <option value={episode.name} key={index}>
             {getFullEpisodeNumber(episode) + ": " + episode.name}
           </option>
         ))}
       </select>
       <button onClick={handleClear}>Clear selection</button>
-      {filteredEpisodes.length !== episodes.length && (
+      {searchTerm && (
         <p>
-          Showing {filteredEpisodes.length} of {episodes.length} episodes
+          Showing {filteredEpisodes.length} of {allEpisodes.length} episodes
         </p>
       )}
       <section style={listStyles}>
